39f58db092f5265d260cff8cae778df1af073044
Saad-7777 <codegurujourney@gmail.com>
2026-07-04 02:53:30 +0500

    solve contains duplicate - hashset_2.0 approach

---
+++ Arrays/Easy/Contains_Duplicate_2.0.py
@@ -0,0 +1,18 @@
+from typing import List
+
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+
+        hashset = set()
+
+        for n in nums:
+            if n in hashset:
+                return True
+            hashset.add(n)
+        return False
+
+
+if __name__ == '__main__':
+    nums = [1, 2, 3, 4]
+    solution = Solution()
+    print(solution.hasDuplicate(nums))
